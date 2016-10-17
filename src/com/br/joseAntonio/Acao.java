3e1c63984e34b9a3f53b9e973c2b00dfbabaf962
jose.silvaneto <jose.silvaneto@lab1-dcx-27>
2016-10-17 08:55:52 -0300

    Primeiro commit ES :D

---
+++ src/com/br/joseAntonio/Acao.java
@@ -0,0 +1,13 @@
+package com.br.joseAntonio;
+
+public class Acao {
+	private Long id;
+	private Double preco;
+	
+	public Acao(Long id, Double preco){
+		this.id = id;
+		this.preco = preco;
+	}
+	
+	
+}
