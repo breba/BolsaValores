fbc9c856f1f0367262838d4f0089da3b315dac1a
sergio.ewerton <sergio.ewerton@lab1-dcx-26>
2016-10-17 09:37:40 -0300

    Criado os GETs e SETs @Sergio

--- src/com/br/joseAntonio/Acao.java
+++ src/com/br/joseAntonio/Acao.java
@@ -2,10 +2,26 @@ package com.br.joseAntonio;
 
 public class Acao {
 	private Long id;
-	private Double preco;
+	private float preco;
 	
-	public Acao(Long id, Double preco){
+	public Acao(Long id, float preco){
 		this.id = id;
+		this.preco = preco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
 		this.preco = preco;
 	}
 	
